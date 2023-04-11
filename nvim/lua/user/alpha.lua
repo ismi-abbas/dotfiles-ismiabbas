c36e4c8b945e1eb806bcaf99c60fd4d0d53e21ef
abbas <muhdabbas98@gmail.com>
2023-04-11 15:19:42 +0800

    Add discord presence plugin

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -22,7 +22,7 @@ dashboard.section.buttons.val = {
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  return "chrisatmachine.com"
+  return "ismi-abbas.com"
 end
 
 dashboard.section.footer.val = footer()
--- nvim/lua/user/toggleterm.lua
+++ nvim/lua/user/toggleterm.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
+size = 20,
 	open_mapping = [[<c-\>]],
 	hide_numbers = true,
 	shade_terminals = true,
